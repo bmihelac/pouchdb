84b9f59ffd79faec5d9678c8f764d2a8b145f039
Nolan Lawson <nolan.lawson@gmail.com>
2014-05-25 10:06:49 -0700

    (#2281) - test against pouchdb-server

--- bin/run-test.sh
+++ bin/run-test.sh
@@ -2,6 +2,19 @@
 
 : ${CLIENT:="node"}
 
+if [[ ! -z $SERVER ]]; then
+
+  if [ "$SERVER" == "pouchdb-server" ]; then
+    export COUCH_HOST='http://127.0.0.1:6984'
+    echo -e "Starting up pouchdb-server\n"
+    ./node_modules/.bin/pouchdb-server -p 6984 &
+  else
+    # I mistype pouchdb-server a lot
+    echo -e "Unknown SERVER $SERVER. Did you mean pouchdb-server?\n"
+    exit 1
+  fi
+fi
+
 if [ "$CLIENT" == "node" ]; then
     npm run test-node
 else
